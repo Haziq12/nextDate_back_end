46c329cc0afb44515a18e397a984f632e1e1ff85
Haziq Naeem <haziqnaeem36@gmail.com>
2022-03-24 14:42:17 -0400

    create profile model

--- models/profile.js
+++ models/profile.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose'
 const profileSchema = new mongoose.Schema({
   email: {type: String, required: true, lowercase: true, unique: true},
   name: String,
+  avatar: String,
+  datePlan: String,
+  aboutMe: String,
+  iceBreakers: String
 },{
     timestamps: true,
 })
